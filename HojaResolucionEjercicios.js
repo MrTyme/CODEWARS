02efbcd46427051bd3ea4296e799c2bb14244a6f
MrTyme <flashcor96.dc@gmail.com>
2020-12-14 17:41:18 -0500

    npm installed

--- HojaResolucionEjercicios.js
+++ HojaResolucionEjercicios.js
@@ -29,5 +29,54 @@ function sumaExp(numbers = []) {
 
 ///////////////////////////////////////////////////////////
 
+//gravity flip.
+
+const flip=(d, a)=>{
+    // la letra "d" la columna del cubo y se le pide que se 
+    // desplace entra derecha o izquiera Right o Left
+
+    if (d === 'R') {
+        return a.sort((a, b) => a-b);
+        //si es a la derecha le pedimos que compare y ordene
+        //siempre recibe una funcion.
+
+        //como se desplaza a la derecha, le pedimos 
+        //que reste la letra "a" a la letra "b"
+    }
+
+    if (d === 'L') {
+        return a.sort((a, b) => b-a);
+    }
+}
+
+/*
+    lo mas comun en este tipo de 
+    ejercicios es utilizar el metodo .sort de los arrays
+    que significa ordenar.
+
+    el cual es un metodo que se usa para ordenar de manera 
+    descendente o acendente, dependiendo de como que coloque
+    el orden de comparacion
+*/
+//solve
 
 
+///////////////////////////////////////////////////////////
+
+
+//Digital root
+
+function digital_root(n) {
+    let i = 0, newArr = new Array();
+    let acum;
+
+    //parseo la variable y la separo con la funcion split
+    newArr[i] = n.toString().split('');
+    console.log(newArr[0]);
+
+    
+    
+}
+
+digital_root(16);
+///////////////////////////////////////////////////////////
